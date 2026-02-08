95dd2a28aca4d6903c75f51584b66e9afe3723af
Abdallahtrd <abdallahnura447@gmail.com>
2026-02-08 22:07:29 +0100

    Initial version: collector and processor function with main orchestration

--- transaction_alert.py
+++ transaction_alert.py
@@ -1,10 +1,52 @@
-balance = 10000
- 
+def user_balance():  #This function collects the balance input from the user.
+    balance = int(input("Input Balance Please: "))
+    return balance
 
-def transaction_amount():
-    name = input("What is your name sir? ")
-    amount = int(input("How much would you like to withdraw: "))
-    total = balance - amount
-    print(name, total)
-transaction_amount()
+def transaction_amount(): #This function collects the amount input from the user.
+    amount = int(input("Please Input Transaction Amount: "))
+    return amount
+
+def transaction_type(): #This function allows for transaction type, Deposit/Withdraw.
+    choice = input("Do you want to withdraw or deposit? ")
+    if choice == "withdraw":
+        print("User choice withdraw.")      
+    elif choice == "deposit":
+        print("User choice deposit.")
+    else:
+        print("Invalid choice")
+    return choice                     
+
+def des_withdraw(amount, balance): #This function calculates for Withdrawal.
+    new_balance = balance - amount
+    return new_balance    
+     
+
+def des_deposit(amount, balance): #This function calculates for Deposit.
+    new_balance = balance + amount
+    return new_balance
+
+def main(): #This is the main function, This is where all the logic is carried out.
+
+#This stores the values from previous functions in variables.    
+    total_balance = user_balance()
+    withdrawal_type = transaction_type()
+    total_amount = transaction_amount()
+     
+
+#This is where the transaction type is processed.
+    if withdrawal_type == "withdraw":
+        new_balance = des_withdraw(total_amount, total_balance)
+    elif withdrawal_type == "deposit":
+        new_balance = des_deposit(total_amount, total_balance)
+    else:
+        print("INVALID REQUEST PLEASE TRY AGAIN")
+        return new_balance
+    print("Your balance is ", new_balance)
+        
+main()
+
+
     
+
+
+
