0c7e4dea4087789a007c5b9263d184c26e4b79ef
Abdallahtrd <abdallahnura447@gmail.com>
2026-02-25 15:12:40 +0100

    Restructured transaction logic and ensured persistent balance.

--- transaction_alert.py
+++ transaction_alert.py
@@ -17,14 +17,13 @@ def transaction_amount(): #This function collects the amount input from the user
     return amount
 
 def transaction_type(): #This function allows for transaction type, Deposit/Withdraw.
-    choice = input("Do you want to withdraw or deposit? use w/d ").lower()
-    if choice == "w":
-        print("User choice withdraw.")     
-    elif choice == "d":
-        print("User choice deposit.")
-    else:
-        print("Invalid choice")
-    return choice                     
+    while True:
+        choice = input("Do you want to withdraw or deposit? use w/d ").lower()
+        if choice == "w" or choice == "d":
+            return choice
+        else:
+            print("Invalid choice. Please Enter 'w' or 'd'.")     
+                         
 
 def des_withdraw(amount, balance): #This function calculates for Withdrawal.
     new_balance = balance - amount
@@ -32,8 +31,8 @@ def des_withdraw(amount, balance): #This function calculates for Withdrawal.
      
 
 def des_deposit(amount, balance): #This function calculates for Deposit.
-    new_balance = balance + amount
-    return new_balance
+    return balance + amount   #I used this version here to show that theres two ways of returning a value(compare to des_withdraw function.)
+
 
 def main(balance): #This is the main function, This is where all the logic is carried out.
 
@@ -47,11 +46,9 @@ def main(balance): #This is the main function, This is where all the logic is ca
 #This is where the transaction type is processed.
     if withdrawal_type == "w":
         new_balance = des_withdraw(total_amount, balance)
-    elif withdrawal_type == "d":
-        new_balance = des_deposit(total_amount, balance)
     else:
-        print("INVALID REQUEST PLEASE TRY AGAIN")
-        return balance #This returns the unchanged balance if the user gives a wrong input.
+       new_balance = des_deposit(total_amount, balance)
+
     print("Your balance is ", new_balance)
     return new_balance #This returns the calculated balance to where the main function is called.
         
@@ -66,6 +63,6 @@ while True: #This is the part that makes the whole program repeatable.
         print("Thank you for using T-Pay!! ")
         break
     else:
-        print("Invalid Input!! Try again. ")
-        break
+        print("Invalid Input!! Please type yes or no. ")
+        
 
